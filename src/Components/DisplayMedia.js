d77111e8154a2f5ac4bcd389dc64d1e129ce84c2
nick.durbin <nick.durbin@gmail.com>
2019-08-29 21:42:20 -0400

    Added some styling.

--- src/Components/DisplayMedia.js
+++ src/Components/DisplayMedia.js
@@ -3,15 +3,15 @@ import ReactPlayer from 'react-player';
 import styled from 'styled-components';
 
 const ImgStyle = styled.img`
-  width: 100%;
-  height: 100%;
+  width: 90%;
+  height: 60vh;
   object-fit: cover;
 `;
 
 function DisplayMedia({ media, url }) {
   return (media === "video")
   ? 
-  <ReactPlayer url={ url } width='100%' height='60vh' alt="NASA" controls='true'/>
+  <ReactPlayer url={ url } width='80%' height='60vh' alt="NASA" controls='true'/>
   :
   <ImgStyle src={ url } alt="NASA" />
 }
